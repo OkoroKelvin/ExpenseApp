ee36bf47286148d4fc5d3d75ea9f42b747db0501
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2022-08-04 07:50:57 +0100

    Implemented Button

--- App.js
+++ App.js
@@ -7,7 +7,9 @@ import RecentExpenses from './screens/RecentExpenses';
 
 import AllExpenses from './screens/AllExpenses';
 import { GlobalStyles } from './constants/styles';
+import IconButton from './components/UI/IconButton';
 import { Ionicons } from '@expo/vector-icons';
+
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -15,12 +17,22 @@ const BottomTabs = createBottomTabNavigator();
 function ExpensesOverview() {
   return (
     <BottomTabs.Navigator
-      screenOptions={{
+    screenOptions={({ navigation }) => ({
       headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
       headerTintColor: 'white',
       tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
       tabBarActiveTintColor: GlobalStyles.colors.accent500,
-    }}
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="add"
+          size={24}
+          color={tintColor}
+          onPress={() => {
+            navigation.navigate('ManageExpense');
+          }}
+        />
+      ),
+    })}
     >
       <BottomTabs.Screen 
       name="RecentExpenses" 
@@ -35,7 +47,7 @@ function ExpensesOverview() {
     />
 
 
-       <BottomTabs.Screen 
+      <BottomTabs.Screen 
        name="AllExpenses" 
        component={AllExpenses}
        options={{
@@ -56,13 +68,22 @@ export default function App() {
     <>
     <StatusBar style="auto" />
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: 'white',
+      }}
+      >
       <Stack.Screen name="ExpensesOverview"
        component={ExpensesOverview}
         options={{ headerShown: false }} />
 
       <Stack.Screen name="ManageExpense"
-       component={ManageExpense} /> 
+       component={ManageExpense}
+       options={{
+        presentation: 'modal',
+      }}
+       /> 
       </Stack.Navigator>
     </NavigationContainer>
     </>
