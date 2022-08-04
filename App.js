c25214a93710aeb201a684a9429dfa45e852798e
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2022-08-04 23:42:54 +0100

    Updated Recent page and All Expenses Page

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import AllExpenses from './screens/AllExpenses';
 import { GlobalStyles } from './constants/styles';
 import IconButton from './components/UI/IconButton';
 import { Ionicons } from '@expo/vector-icons';
+import ExpensesContextProvider from './store/expenses-context';
 
 
 const Stack = createNativeStackNavigator();
@@ -67,6 +68,7 @@ export default function App() {
   return(
     <>
     <StatusBar style="auto" />
+    <ExpensesContextProvider>
     <NavigationContainer>
       <Stack.Navigator
       screenOptions={{
@@ -86,6 +88,7 @@ export default function App() {
        /> 
       </Stack.Navigator>
     </NavigationContainer>
+    </ExpensesContextProvider>
     </>
 
   );
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,15 @@
-import { useLayoutEffect } from 'react';
-import { Text } from 'react-native';
+import { useLayoutEffect,useContext } from 'react';
+import {  StyleSheet, View } from 'react-native';
+import { GlobalStyles } from '../constants/styles';
+import Button from '../components/UI/Button';
+import IconButton from '../components/UI/IconButton';
+import { ExpensesContext } from '../store/expenses-context';
 
 function ManageExpense({ route, navigation }) {
+
+  const expensesCtx = useContext(ExpensesContext);
+
+
   const editedExpenseId = route.params?.expenseId;  
   const isEditing = !!editedExpenseId;
 
@@ -11,7 +19,83 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  return <Text>ManageExpense Screen</Text>;
+  function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(
+        editedExpenseId,
+        {
+          description: 'Test!!!!',
+          amount: 29.99,
+          date: new Date('2022-08-04'),
+        }
+      );
+    } else {
+      expensesCtx.addExpense({
+        description: 'Test',
+        amount: 19.99,
+        date: new Date('2022-05-19'),
+      });
+    }
+    navigation.goBack();
+  }
+
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler} >
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={25}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
