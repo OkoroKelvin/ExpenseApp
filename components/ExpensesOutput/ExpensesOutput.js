0925df00df4e7fb9b12f8a9693179c955ad5d21c
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2022-08-05 01:21:52 +0100

    Completed Project

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,4 +1,4 @@
-import { View , StyleSheet} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
 
 import ExpensesList from './ExpensesList';
@@ -6,11 +6,17 @@ import ExpensesSummary from './ExpensesSummary';
 
 
 
-function ExpensesOutput({ expenses, expensesPeriod }) {
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+
+  if (expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />;
+  }
+
   return (
     <View style={styles.container}>
-    <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-    <ExpensesList expenses={expenses} />
+     <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      {content}
   </View>
   );
 }
@@ -20,7 +26,16 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    backgroundColor: GlobalStyles.colors.primary700
-  }
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 32,
+  },
 });
+
